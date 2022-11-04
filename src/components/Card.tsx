92b210bad845d6332584f73763d09b395a700628
Joseph <joseph300905@gmail.com>
2022-11-04 10:53:30 +0100

    Better card display

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,6 @@
 import { Component } from "solid-js";
 
-export const grid = 'grid grid-cols-6 h-72 gap-8 m-8';
+export const grid = 'grid grid-cols-5 h-64 gap-8 m-8';
 
 export const Card: Component<{ content: string, onClick?: Function }> = (props) => {
   return <div onClick={() => props.onClick ? props.onClick() : {}} class="cursor-pointer shadow-md rounded-xl bg-gray-700 p-6 hover:-translate-y-1 hover:shadow-lg transition-all">
